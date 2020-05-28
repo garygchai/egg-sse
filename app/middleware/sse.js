806e3accef208050eb863ab2622e15b5750595e5
changhai.gch <changhai.gch@alibaba-inc.com>
2020-05-28 10:56:09 +0800

    支持多实例

--- app/middleware/sse.js
+++ app/middleware/sse.js
@@ -3,21 +3,33 @@
 const SSE = require('../../sse');
 module.exports = () => {
   return async function sse_call(ctx, next) {
-    const opts = ctx.app.config.sse;
-    if (ctx.path.indexOf(opts.path) > -1) {
-      if (ctx.res.headerSent) {
-        if (!(ctx.sse instanceof SSE)) {
-          console.error('SSE response header has been sent, Unable to create the sse response');
+    let opts = { client: ctx.app.config.sse };
+    if (ctx.app.config.sse.client) {
+      opts = ctx.app.config.sse;
+    } else if (ctx.app.config.sse.clients) {
+      opts = ctx.app.config.sse.clients;
+    }
+    for (const i in opts) {
+      const config = Object.assign({}, opts[i], ctx.app.config.sse.default);
+      let sseClient = ctx.app.sse;
+      if (!sseClient.ssePool) {
+        sseClient = ctx.app.sse.get(i);
+      }
+      if (ctx.path === config.path) {
+        if (ctx.res.headerSent) {
+          if (!(ctx.sse instanceof SSE)) {
+            console.error('SSE response header has been sent, Unable to create the sse response');
+          }
+          return await next();
         }
-        return await next();
+        if (sseClient.ssePool.length >= config.maxClients) {
+          console.error('SSE client number more than the maximum, Unable to create the sse response');
+          return await next();
+        }
+        const sseItem = new SSE(ctx, config);
+        ctx.sse = ctx.response.sse = sseItem;
+        sseClient.ssePool.push(sseItem);
       }
-      if (ctx.app.sse.ssePool.length >= opts.maxClients) {
-        console.error('SSE client number more than the maximum, Unable to create the sse response');
-        return await next();
-      }
-      const sseItem = new SSE(ctx, opts);
-      ctx.sse = ctx.response.sse = sseItem;
-      ctx.app.sse.ssePool.push(sseItem);
     }
     await next();
   };
--- config/config.default.js
+++ config/config.default.js
@@ -2,16 +2,18 @@
 'use strict';
 module.exports = {
   sse: {
-    path: '/stream',
-    headers: {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache, no-transform',
-      'Connection': 'keep-alive',
-      'X-Accel-Buffering': 'no',
-      'Transfer-Encoding': 'off',
-      'Access-Control-Allow-Origin': '*',
-    },
-    maxClients: 10000,
-    pingInterval: 10000,
+    client: {
+      path: '/stream',
+      headers: {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache, no-transform',
+        'Connection': 'keep-alive',
+        'X-Accel-Buffering': 'no',
+        'Transfer-Encoding': 'off',
+        'Access-Control-Allow-Origin': '*',
+      },
+      maxClients: 10000,
+      pingInterval: 10000,
+    }
   },
 };
